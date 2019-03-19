632887eac9294d0059e4740d89d9497cd2e321db
Angus <angushon96@gmail.com>
2019-03-19 05:35:28 -0700

    added contact page, svgs
    editted svg colours manually

--- port/src/App.js
+++ port/src/App.js
@@ -1,6 +1,9 @@
 import React, { Component } from 'react';
 import { CSSTransitionGroup } from 'react-transition-group';
-import logo from './logo.svg'
+import logo from './assets/logo.svg'
+import linkedinIcon from './assets/linkedin.svg'
+import emailIcon from './assets/email.svg'
+import phoneIcon from './assets/chat.svg'
 import './css/App.css';
 import './css/palette.css'
 
@@ -22,8 +25,12 @@ class App extends React.Component {
       case "About":
         component = this.aboutPage();
         break;
+      case "Contact":
+        component = this.contactPage();
+        break;
       case "Default":
         component = this.defaultPage();
+        break;
     }
     return component;
   }
@@ -32,11 +39,11 @@ class App extends React.Component {
     this.setState({
       currentComponent: pageName
     });
-    if(pageName !== "Default"){
+    if (pageName !== "Default") {
       this.setState({
         headerExpand: true
       });
-    }else{
+    } else {
       this.setState({
         headerExpand: false
       })
@@ -87,19 +94,41 @@ class App extends React.Component {
     );
   }
 
+  contactPage() {
+    return (
+      <React.Fragment>
+        <section className="Contact bgGrey">
+          <a href="https://www.linkedin.com/in/angus-hon/" target="_blank" rel="noopener noreferrer">
+            <img className="Contact-icons" src={linkedinIcon} alt="linkedin logo made by Freepik" />
+            <span>https://www.linkedin.com/in/angus-hon/</span>
+          </a>
+          <a>
+            <img className="Contact-icons" src={emailIcon} alt="email icon made by Freepik" />
+            <span>angus_hon@sfu.ca</span>
+          </a>
+          <a>
+            <img className="Contact-icons" src={phoneIcon} alt="phone icon made by Zurb" />
+            <span>+1 (778) 788 9168</span>
+          </a>
+
+          <div className="Icon-reference">Icons made by <a href="https://www.flaticon.com/authors/freepik" title="Freepik">Freepik</a> and <a href="https://www.flaticon.com/<?=_('authors').'/'?>zurb" title="Zurb">Zurb</a> from <a href="https://www.flaticon.com/" title="Flaticon">www.flaticon.com</a> is licensed by <a href="http://creativecommons.org/licenses/by/3.0/" title="Creative Commons BY 3.0" target="_blank">CC 3.0 BY</a></div>        </section>
+      </React.Fragment>
+    );
+  }
+
   render() {
     return (
       <div className="App bgAlmostBlack">
         <header className={this.state.headerExpand ? "App-header-expanded" : "App-header-compressed"}>
-          <img className={this.state.headerExpand ? "App-logo App-logo-pin": "App-logo"} onClick={() => this.nextComponent("Default")} 
-            src={logo} alt="temp react logo"/>
+          <img className={this.state.headerExpand ? "App-logo App-logo-pin" : "App-logo"} onClick={() => this.nextComponent("Default")}
+            src={logo} alt="temp react logo" />
           <nav className="Navigation">
-            <span className="colBlue" onClick={() => this.nextComponent("About")}>About</span>
+            <span className={this.state.currentComponent === "About" ? "colNeonBlue" : "colBlue"} onClick={() => this.nextComponent("About")}>About</span>
             <span className="colBlue">Projects</span>
-            <span className="colBlue">Contact</span>
+            <span className={this.state.currentComponent === "Contact" ? "colNeonBlue" : "colBlue"} onClick={() => this.nextComponent("Contact")}>Contact</span>
           </nav>
         </header>
-        <div className="Container">
+        <div className={this.state.headerExpand ? "Container Container-expanded" : "Container"}>
           {this.getComponent()}
         </div>
       </div>
