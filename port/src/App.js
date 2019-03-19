d5ce32776676d2d2702bf92900aca5e1156b1c1b
Angus <angushon96@gmail.com>
2019-03-19 06:40:10 -0700

    added motionart project to projects section

--- port/src/App.js
+++ port/src/App.js
@@ -4,6 +4,11 @@ import logo from './assets/logo.svg'
 import linkedinIcon from './assets/linkedin.svg'
 import emailIcon from './assets/email.svg'
 import phoneIcon from './assets/chat.svg'
+import motionArt1 from './assets/motionart1.jpg'
+import motionArt2 from './assets/motionart2.jpg'
+import motionArt3 from './assets/motionart3.jpg'
+import motionArt4 from './assets/motionart4.jpg'
+import motionArt5 from './assets/motionart5.jpg'
 import './css/App.css';
 import './css/palette.css'
 
@@ -25,6 +30,9 @@ class App extends React.Component {
       case "About":
         component = this.aboutPage();
         break;
+      case "Projects":
+        component = this.projectsPage();
+        break;
       case "Contact":
         component = this.contactPage();
         break;
@@ -39,6 +47,7 @@ class App extends React.Component {
     this.setState({
       currentComponent: pageName
     });
+    // if header is expanded, container expanded to 100% height 
     if (pageName !== "Default") {
       this.setState({
         headerExpand: true
@@ -62,6 +71,7 @@ class App extends React.Component {
     return (
       <React.Fragment>
         <section className="About bgGrey">
+          <h1>5W + 1H = Angus H.</h1>
           <div className="About-info">
             <h2>Who is Angus?</h2>
             <p>He's a data scientist / software engineer to be.</p>
@@ -94,10 +104,45 @@ class App extends React.Component {
     );
   }
 
+  projectsPage() {
+    return (
+      <React.Fragment>
+        <section className="Projects bgGrey">
+          <h1>Things I've worked on</h1>
+          <h2>Motion Art</h2>
+          <p>The idea for this mobile application dervies from the attempt to enable a means of drawing through your body's
+            movement. My goal was to map device position to a blank canvas in which the user's movement would translate as a drawing stroke.
+            However, given that this was a project for school I was restricted to only the use of the sensors native to mobile devices.
+            This proved to be a limitation for what I had hoped to achieve.
+          </p>
+          <div className="Projects-gallery">
+            <img src={motionArt1} alt="pic of motionart's brush select" />
+            <img src={motionArt2} alt="pic of motionart's palette editor" />
+            <img src={motionArt3} alt="pic of motionart's base screen" />
+            <img src={motionArt4} alt="pic of motionart's artboard renaming" />
+            <img src={motionArt5} alt="pic of motionart with sample art" />
+          </div>
+          <p>The native sensors that I experimented with include the accelerometer, gyroscope, and GPS.</p>
+          <ul>
+            <li>Mathematical limitations with the accelerometer pertain to the inaccuracies of deriving position from double integration.</li>
+            <li>GPS has various levels of accuracy. Low accuracy is too imprecise with accuracys within 50m while high accuracy
+              delivers accurate positioning within 5m but takes too much time to acquire data and too much power consumption.
+            </li>
+            <li>Gyroscope itself can determine device orientation</li>
+          </ul>
+
+          <p>Attempted gyroscope + accelerometer combination but still too inaccurate.</p>
+          <p>At the end of the day, what I achieved was a basic drawing application without the special / intended feature.</p>
+        </section>
+      </React.Fragment>
+    );
+  }
+
   contactPage() {
     return (
       <React.Fragment>
         <section className="Contact bgGrey">
+          <h1>Grab a hold of me here!</h1>
           <a href="https://www.linkedin.com/in/angus-hon/" target="_blank" rel="noopener noreferrer">
             <img className="Contact-icons" src={linkedinIcon} alt="linkedin logo made by Freepik" />
             <span>https://www.linkedin.com/in/angus-hon/</span>
@@ -124,7 +169,7 @@ class App extends React.Component {
             src={logo} alt="temp react logo" />
           <nav className="Navigation">
             <span className={this.state.currentComponent === "About" ? "colNeonBlue" : "colBlue"} onClick={() => this.nextComponent("About")}>About</span>
-            <span className="colBlue">Projects</span>
+            <span className={this.state.currentComponent === "Projects" ? "colNeonBlue" : "colBlue"} onClick={() => this.nextComponent("Projects")}>Projects</span>
             <span className={this.state.currentComponent === "Contact" ? "colNeonBlue" : "colBlue"} onClick={() => this.nextComponent("Contact")}>Contact</span>
           </nav>
         </header>
