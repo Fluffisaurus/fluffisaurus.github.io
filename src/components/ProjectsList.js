d3afdf162dfb9d69c93560d89485cd398c6d6544
Angus Hon - PC <angushon96@gmail.com>
2020-01-05 01:09:09 -0800

    ProjectLists json data structure

---
+++ src/components/ProjectsList.js
@@ -0,0 +1,198 @@
+export const projects = {
+    "template" : {
+        0 : {
+            "abbr": "",
+            "name": "",
+            "date" : "",
+            "tags" : [""],
+            "category" : "",
+            "link": {
+                "repo" : "",
+                "host" : "",
+            },
+            "detail": {
+                "short" : { //one to two sentence summary, concise!
+
+                },
+                "long" : { //two paragraph summary
+
+                }
+            }
+        },
+    },
+    "personal": {
+        0 : {
+            "abbr": "nba-data-viz",
+            "name": "NBA Data Visualization",
+            "date" : "Aug 2019 - Present",
+            "tags" : ["D3", "React", "Python", "Flask", "Javascript", "CSS", "Git"],
+            "category" : "Web App / Tool",
+            "link": {
+                "repo" : "",
+                "host" : "",
+            },
+            "detail": {
+                "short" : { //one to two sentence summary, concise!
+
+                },
+                "long" : { //two paragraph summary
+
+                }
+            }
+        },
+        1 : {
+            "abbr": "website",
+            "name": "Personal Website",
+            "date" : "Mar 2019 - Present",
+            "tags" : ["React", "Javascript", "CSS", "Git"],
+            "category" : "Single-Page Web Application",
+            "link": {
+                "repo" : "",
+                "host" : "",
+            },
+            "detail": {
+                "short" : { //one to two sentence summary, concise!
+
+                },
+                "long" : { //two paragraph summary
+
+                }
+            }
+        },
+        2 : {
+            "abbr": "foodpick",
+            "name": "FoodPick",
+            "date" : "Jun 2018 - Mar 2019",
+            "tags" : ["React", "Redux", "Javascript", "NodeJS", "CSS", "Git"],
+            "category" : "Web and Mobile App",
+            "link": {
+                "repo" : "https://github.com/kca99/foodpick",
+                "host" : "",
+            },
+            "detail": {
+                "short" : { //one to two sentence summary, concise!
+
+                },
+                "long" : { //two paragraph summary
+
+                }
+            }
+        },
+        3 : {
+            "abbr": "falling-blocks",
+            "name": "Falling Blocks",
+            "date" : "Apr 2018 - May 2018",
+            "tags" : ["C#", "Unity", "Game Dev"],
+            "category" : "Endless Game",
+            "link": {
+                "repo" : "https://github.com/Fluffisaurus/Falling-Blocks",
+                "host" : "",
+            },
+            "detail": {
+                "short" : { //one to two sentence summary, concise!
+
+                },
+                "long" : { //two paragraph summary
+
+                }
+            }
+        },
+    },
+    "academic" : {
+        0 : {
+            "abbr": "bobbys-breakout",
+            "name": "Bobby's Breakout",
+            "date" : "Sep 2019 - Dec 2019",
+            "tags" : ["Agile", "Full Stack", "NodeJS", "Heroku", "Object Oriented Design", "Game Engine", "System Architecture", "Git"],
+            "category" : "Top Down RPG",
+            "link" : {
+                "repo"  : "https://github.com/abhatthal/Bobby-Breakout",
+                "host" : "https://project-276.herokuapp.com/",
+            },
+            "detail": {
+                "short" : { //one to two sentence summary, concise!
+
+                },
+                "long" : { //two paragraph summary
+
+                }
+            }
+        },
+        1 : {
+            "abbr": "player-performance-projection",
+            "name": "Player Performance Projection",
+            "date" : "Sep 2019 - Dec 2019",
+            "tags" : ["Python", "Machine Learning", "ANOVA", "Regression", "Data Science", "Data Cleaning", "Data Preprocessing"],
+            "category" : "Statistical Analysis",
+            "link": {
+                "repo" : "https://github.com/Fluffisaurus/player-performance-projection",
+                "host" : "https://github.com/Fluffisaurus/player-performance-projection/blob/master/report.md",
+            },
+            "detail": {
+                "short" : { //one to two sentence summary, concise!
+
+                },
+                "long" : { //two paragraph summary
+
+                }
+            }
+        },
+        2 : {
+            "abbr": "motion-art",
+            "name": "Motion Art",
+            "date" : "Sep 2018 - Dec 2018",
+            "tags" : ["Java", "Android Studio", "Sensors", "Activity Lifecycle"],
+            "category" : "Android App",
+            "link": {
+                "repo" : "",
+                "host" : "",
+            },
+            "detail": {
+                "short" : { //one to two sentence summary, concise!
+
+                },
+                "long" : { //two paragraph summary
+
+                }
+            }
+        },
+        3 : {
+            "abbr": "kingdom-of-helios",
+            "name": "Kingdom of Helios",
+            "date" : "Oct 2018 - Dec 2018",
+            "tags" : ["Unity", "C#", "Game Dev", "Game Design", "Git"],
+            "category" : "2D Side-Scrolling Platformer Game",
+            "link": {
+                "repo" : "https://github.com/Fluffisaurus/kingdom-of-helios",
+                "host" : "",
+            },
+            "detail": {
+                "short" : { //one to two sentence summary, concise!
+
+                },
+                "long" : { //two paragraph summary
+
+                }
+            }
+        },
+        4 : {
+            "abbr": "antlion-td",
+            "name": "Antlion Tower Defense",
+            "date" : "Oct 2018 - Dec 2018",
+            "tags" : ["Unity", "C#", "Pathfinding", "Wave Spawner System", "Game Design", "Game Dev", "Git"],
+            "category" : "Tower Defense Game",
+            "link": {
+                "repo" : "https://github.com/Fluffisaurus/ATD",
+                "host" : "",
+            },
+            "detail": {
+                "short" : { //one to two sentence summary, concise!
+
+                },
+                "long" : { //two paragraph summary
+
+                }
+            }
+        },
+    },
+};
