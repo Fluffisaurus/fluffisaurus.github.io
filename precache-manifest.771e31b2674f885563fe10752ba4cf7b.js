9a6edff93e89136723ad3b8eb19a61ba6899957b
Angus Stealth <angushon96@gmail.com>
2020-02-02 22:34:41 -0800

    Website upload

---
+++ precache-manifest.771e31b2674f885563fe10752ba4cf7b.js
@@ -0,0 +1,238 @@
+self.__precacheManifest = [
+  {
+    "revision": "8e4b147428f261cfece8",
+    "url": "/pport/static/css/main.0315a7d5.chunk.css"
+  },
+  {
+    "revision": "8e4b147428f261cfece8",
+    "url": "/pport/static/js/main.b2cd0470.chunk.js"
+  },
+  {
+    "revision": "064b534c463f0ee72c8d",
+    "url": "/pport/static/js/runtime~main.288178b2.js"
+  },
+  {
+    "revision": "986da9b6ef9d76ad5c66",
+    "url": "/pport/static/css/2.fb7eeb3d.chunk.css"
+  },
+  {
+    "revision": "986da9b6ef9d76ad5c66",
+    "url": "/pport/static/js/2.aedd37d6.chunk.js"
+  },
+  {
+    "revision": "b0cb945cee076573498e432e20c64268",
+    "url": "/pport/static/media/lightbulb-off.b0cb945c.svg"
+  },
+  {
+    "revision": "e8cede189854bb4c7de81cde73adc879",
+    "url": "/pport/static/media/GitHub-Mark.e8cede18.svg"
+  },
+  {
+    "revision": "0f432e726724edcd07d4aac19679ace4",
+    "url": "/pport/static/media/email.0f432e72.svg"
+  },
+  {
+    "revision": "93be05523387d178daf7eb0bb72fe5f0",
+    "url": "/pport/static/media/linkedin.93be0552.svg"
+  },
+  {
+    "revision": "4ef35bc8f13c8959729dfd78bf760da2",
+    "url": "/pport/static/media/lightbulb.4ef35bc8.svg"
+  },
+  {
+    "revision": "8e5c090b22a9042abb1f937b1d795629",
+    "url": "/pport/static/media/myLogo2.8e5c090b.svg"
+  },
+  {
+    "revision": "a297f2a994130ad73414ac7904075384",
+    "url": "/pport/static/media/placeholderGallery.a297f2a9.png"
+  },
+  {
+    "revision": "ad89504315900269a9b7bf4670358ce8",
+    "url": "/pport/static/media/atd-gameplay.ad895043.png"
+  },
+  {
+    "revision": "c9b22dbabf03f7603aaf2dfcbc1ef9d8",
+    "url": "/pport/static/media/atd-level-select.c9b22dba.png"
+  },
+  {
+    "revision": "7f8ea519ad29d6fa7e321b7e65ff24f9",
+    "url": "/pport/static/media/atd-lose-screen.7f8ea519.png"
+  },
+  {
+    "revision": "a98e2f2a689d2d6c72fd2c0c970abb59",
+    "url": "/pport/static/media/atd-pathfinding.a98e2f2a.png"
+  },
+  {
+    "revision": "e9850be1f85a32a16a924081676cfae7",
+    "url": "/pport/static/media/atd-start-screen.e9850be1.png"
+  },
+  {
+    "revision": "69b35cfcf376ba2238eb9dff18e70f9f",
+    "url": "/pport/static/media/atd-story.69b35cfc.png"
+  },
+  {
+    "revision": "462da03ef29bd1d223b9d5ea335c4999",
+    "url": "/pport/static/media/atd-win-screen.462da03e.png"
+  },
+  {
+    "revision": "ee2ffbcb0e2c14dd50fe04c132e1de24",
+    "url": "/pport/static/media/bobby-breakout-achievement.ee2ffbcb.png"
+  },
+  {
+    "revision": "0a9c0fd406194fb03fadf197c9d4bd95",
+    "url": "/pport/static/media/bobby-breakout-fight-1.0a9c0fd4.png"
+  },
+  {
+    "revision": "71fa727cb4ea9e8f68425a3e4d4d62ea",
+    "url": "/pport/static/media/bobby-breakout-fight-2.71fa727c.png"
+  },
+  {
+    "revision": "d69bd521e032cd634a1a6f22da25850c",
+    "url": "/pport/static/media/bobby-breakout-fight.d69bd521.png"
+  },
+  {
+    "revision": "f55754e23fb2d8bb78c7562dbd5e81ca",
+    "url": "/pport/static/media/bobby-breakout-inventory.f55754e2.png"
+  },
+  {
+    "revision": "6327402ff4481e85c0c2be60ce7ebea3",
+    "url": "/pport/static/media/bobby-breakout-item.6327402f.png"
+  },
+  {
+    "revision": "d3a61a3638ca6a1ea8b2cee2c8168c65",
+    "url": "/pport/static/media/bobby-breakout-login.d3a61a36.png"
+  },
+  {
+    "revision": "7aa92e4f9c19d9c78775a13454a537f0",
+    "url": "/pport/static/media/bobby-breakout-map.7aa92e4f.png"
+  },
+  {
+    "revision": "59b9a03cb0d0aa8ae4ba0e015a821c17",
+    "url": "/pport/static/media/fbdv-assists.59b9a03c.png"
+  },
+  {
+    "revision": "3e3b3ded4654c56a7ee5bbe5cc5a6380",
+    "url": "/pport/static/media/fbdv-brush.3e3b3ded.png"
+  },
+  {
+    "revision": "233fb0289b8a479bbb71469c18761eb2",
+    "url": "/pport/static/media/fbdv-tooltip.233fb028.png"
+  },
+  {
+    "revision": "ff7f6b2cf42eef520553213e60847cf8",
+    "url": "/pport/static/media/fbdv-zoom-1.ff7f6b2c.png"
+  },
+  {
+    "revision": "f020a575ca8f862924b2031343a50388",
+    "url": "/pport/static/media/fbdv-zoom.f020a575.png"
+  },
+  {
+    "revision": "8d341c7742619ffb9bcb4da41a87ab75",
+    "url": "/pport/static/media/char-x.8d341c77.PNG"
+  },
+  {
+    "revision": "56696cb9a127e2ef55f27a2f557c84ff",
+    "url": "/pport/static/media/char-y.56696cb9.PNG"
+  },
+  {
+    "revision": "f142ff7286afc391658d4020d4670c55",
+    "url": "/pport/static/media/main-screen.f142ff72.png"
+  },
+  {
+    "revision": "ae9e41b12c2e018f5e90968c49e5ad70",
+    "url": "/pport/static/media/scene-flow-chart.ae9e41b1.png"
+  },
+  {
+    "revision": "df2d17fd9a836c6421f69be521b7402f",
+    "url": "/pport/static/media/story-map.df2d17fd.png"
+  },
+  {
+    "revision": "fd1b1a6bc7afa022c8d2d478ade7520b",
+    "url": "/pport/static/media/hexagon1.fd1b1a6b.svg"
+  },
+  {
+    "revision": "653ded3eef8e835bdb7823f5b2705ff0",
+    "url": "/pport/static/media/hexagon2.653ded3e.svg"
+  },
+  {
+    "revision": "24cd436b0d878afce1570ae11898633a",
+    "url": "/pport/static/media/hexagon3.24cd436b.svg"
+  },
+  {
+    "revision": "11fbaebdafd7b80f983208cd7c804376",
+    "url": "/pport/static/media/motionart1.11fbaebd.jpg"
+  },
+  {
+    "revision": "e61ed48bc53406334a2aad8b11bee2ba",
+    "url": "/pport/static/media/motionart2.e61ed48b.jpg"
+  },
+  {
+    "revision": "4f9e629d63fa72b97cd488add717f146",
+    "url": "/pport/static/media/motionart3.4f9e629d.jpg"
+  },
+  {
+    "revision": "5d976a302cd8710c17d2a89311365a6b",
+    "url": "/pport/static/media/motionart4.5d976a30.jpg"
+  },
+  {
+    "revision": "e694baf3cbe3668263751a6a1a62201c",
+    "url": "/pport/static/media/motionart5.e694baf3.jpg"
+  },
+  {
+    "revision": "799dc05bc65f1152ff6852dbd15a62b9",
+    "url": "/pport/static/media/myLogo.799dc05b.svg"
+  },
+  {
+    "revision": "8e5b7bfbf0e7d2849b8b187b1669d608",
+    "url": "/pport/static/media/all-3-def-tiers-train-vs-predict.8e5b7bfb.png"
+  },
+  {
+    "revision": "27b5dc12eb8a1024e21ead98ede85f39",
+    "url": "/pport/static/media/curry-shots-made-by-minutes.27b5dc12.png"
+  },
+  {
+    "revision": "17da0a364c0fcee7e346a08c747385d3",
+    "url": "/pport/static/media/plot-nba-def-rating.17da0a36.png"
+  },
+  {
+    "revision": "e2875c11668df789df7276a19f255c37",
+    "url": "/pport/static/media/variance-shot-defs.e2875c11.png"
+  },
+  {
+    "revision": "12fdb093e6560878f33879c29c126f9f",
+    "url": "/pport/static/media/summer-max.12fdb093.png"
+  },
+  {
+    "revision": "105992d8a68107047f70e4905082ffd3",
+    "url": "/pport/static/media/summer-min.105992d8.png"
+  },
+  {
+    "revision": "40c3c2064e3d835d4a741d524c348bff",
+    "url": "/pport/static/media/mobile-about.40c3c206.jpg"
+  },
+  {
+    "revision": "ab0d60ed7853e4eab3839c8facaa250e",
+    "url": "/pport/static/media/mobile-light-about.ab0d60ed.jpg"
+  },
+  {
+    "revision": "f50aa0655b9fb2f812701693125a5537",
+    "url": "/pport/static/media/mobile-light-projects.f50aa065.jpg"
+  },
+  {
+    "revision": "8620cfebfe14849a91d82acf499d28aa",
+    "url": "/pport/static/media/mobile-light.8620cfeb.jpg"
+  },
+  {
+    "revision": "2a1108ae4cdc44d5caf9a0c37691debb",
+    "url": "/pport/static/media/mobile-projects.2a1108ae.jpg"
+  },
+  {
+    "revision": "1b1c054fac487b53e6a11d9666142539",
+    "url": "/pport/static/media/mobile.1b1c054f.jpg"
+  },
+  {
+    "revision": "4cca9e0f77bd5f3050ce7db0e59d807b",
+    "url": "/pport/index.html"
+  }
+];
