5d995678418dd48a6c7454788d5161eeadae6710
Angus-Blade-Stealth <angushon96@gmail.com>
2022-05-26 00:59:20 -0700

    shorter DNE content

--- src/pages/Defaults.tsx
+++ src/pages/Defaults.tsx
@@ -5,11 +5,8 @@ export const DoesNotExist: React.FunctionComponent = () => {
     <>
       <section className="Page bgAccent2">
         <h1>Oops!</h1>
-        <p>
-          The URL you typed in does not exist... yet? Who knows, maybe it will
-          one day. Perhaps you misspelled it? If not, I'm as lost as you are.
-        </p>
-        <p>Use the navigation above to go to an existing page!</p>
+        <p>The page you're looking for does not exist (yet?)!</p>
+        <p>Click the links above to teleport back!</p>
       </section>
     </>
   );
