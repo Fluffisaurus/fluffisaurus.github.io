106dcbe9f80d1fc721607d99cda3fe91e718f40a
Angus-Blade-Stealth <angushon96@gmail.com>
2022-05-27 23:08:34 -0700

    update landing page components and content

--- src/pages/Landing.tsx
+++ src/pages/Landing.tsx
@@ -1,28 +1,40 @@
 import React from "react";
 import { Outlet, Link } from "react-router-dom";
 
+import "../styles/landing.scss";
+
 const Landing: React.FunctionComponent = () => {
   return (
     <div className="Container">
       <div className="Container__greeting">
-        <p>Hi there fellow human(s)!</p>
+        <span>
+          <p>Hi there fellow human! I am</p>
+        </span>
       </div>
       <div className="Container__name">
-        <span>
+        <span className="Container__name__fullname">
           <h1>angus_hon</h1>
         </span>
-        <span>
+        <span className="Container__name__at">
           <h1>@</h1>
         </span>
-        <span>
+        <span className="Container__name__title">
           <h1>software_engineer</h1>
         </span>
       </div>
       <nav className="Container__nav">
-        <span>~$</span>
-        <Link to="/default">default</Link>
-        <Link to="/about">about</Link>
-        <Link to="/contact">contact</Link>
+        <span>
+          <a>~$</a>
+        </span>
+        <span>
+          <Link to="/default">default</Link>
+        </span>
+        <span>
+          <Link to="/about">about</Link>
+        </span>
+        <span>
+          <Link to="/contact">contact</Link>
+        </span>
       </nav>
       <Outlet />
     </div>
