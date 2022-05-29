a94804e81c6520c0a926d1df8c79801026c3b20e
Angus-Blade-Stealth <angushon96@gmail.com>
2022-05-29 01:26:05 -0700

    ContentBlock, cleaner way of content management

---
+++ src/components/ContentBlock.tsx
@@ -0,0 +1,29 @@
+import React, { useEffect, useState } from "react";
+
+import "../styles/content-block.scss";
+
+export interface ContentBlockProps {
+  heading: string;
+  body: string[];
+}
+
+const ContentBlock = ({ heading, body }: ContentBlockProps) => {
+  const [headingText, setHeadingText] = useState<string>("");
+  const [bodyText, setBodyText] = useState<string[]>([""]);
+
+  useEffect(() => {
+    setHeadingText(heading);
+    setBodyText(body);
+  }, [body, heading]);
+
+  return (
+    <div className="Content-Block">
+      <h2>{headingText}</h2>
+      {bodyText.map((body, i) => {
+        return <p key={i}>{body}</p>;
+      })}
+    </div>
+  );
+};
+
+export default ContentBlock;
---
+++ src/content/about-me.tsx
@@ -0,0 +1,38 @@
+import { ContentBlockProps } from "../components/ContentBlock";
+
+const AboutMeContent: Array<ContentBlockProps> = [
+  {
+    heading: "Who is Angus?",
+    body: [
+      "He is a software engineer who also happens to be a data scientist. He is also an avid computer & keyboard builder and amateur basketball analyst.",
+    ],
+  },
+  {
+    heading: "What is Angus?",
+    body: [
+      "He is an ethnically Chinese human that was born and raised in Vancouver, Canada. As noted by culinary experts, Angus is also a type of beef.",
+    ],
+  },
+  {
+    heading: "Where is Angus?",
+    body: ["He is currently in Vancouver, BC, Canada."],
+  },
+  {
+    heading: "When is Angus?",
+    body: ["HE IS NOW."],
+  },
+  {
+    heading: "Why is Angus?",
+    body: ["That is a question he frequently asks himself."],
+  },
+  {
+    heading: "How is Angus?",
+    body: [
+      "He is very busy splitting his time as a Software Engineer at Sokanu and an undergrad student at Simon Fraser University.",
+      "He is currently finishing his last semester of school - main'ing in Data Science and alt'ing Interactive Arts and Technology (Game Design and Interaction Design).",
+      "In his spare time, Angus enjoys hanging out with his friends, playing video games, and watching the NBA.",
+    ],
+  },
+];
+
+export default AboutMeContent;
