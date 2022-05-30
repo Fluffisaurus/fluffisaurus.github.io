4bd7c4761443b29f758ceda30d8670d056f1297a
Angus-Blade-Stealth <angushon96@gmail.com>
2022-05-30 02:20:26 -0700

    did a bunch of bs for no reason lmao wtf

--- src/pages/About.tsx
+++ src/pages/About.tsx
@@ -1,20 +1,14 @@
-import React, { useEffect, useState } from "react";
+import React from "react";
 
 import AboutMeContent from "../content/about-me";
-import ContentBlock, { ContentBlockProps } from "../components/ContentBlock";
+import ContentBlock from "../components/ContentBlock";
 
 const AboutMe: React.FunctionComponent = () => {
-  const [contentData, setContentData] = useState<Array<ContentBlockProps>>();
-
-  useEffect(() => {
-    setContentData(AboutMeContent);
-  });
-
   return (
     <>
       <section>
         <h1>5w1h</h1>
-        {contentData?.map((cntBlk, i) => {
+        {AboutMeContent?.map((cntBlk, i) => {
           return (
             <ContentBlock key={i} heading={cntBlk.heading} body={cntBlk.body} />
           );
