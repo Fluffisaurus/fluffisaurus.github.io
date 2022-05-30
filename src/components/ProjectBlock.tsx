e8ca3170dfc34bacadf8c46193cb062d18c7ec52
Angus-Blade-Stealth <angushon96@gmail.com>
2022-05-30 02:16:30 -0700

    generic projectblock, no styling yet

---
+++ src/components/ProjectBlock.tsx
@@ -0,0 +1,29 @@
+import React, { useEffect, useState } from "react";
+
+import { Project } from "../content/projects/interfaces";
+
+import "../styles/project-block.scss";
+
+interface ProjectBlockProps {
+  proj: Project;
+}
+
+const ProjectBlock = ({ proj }: ProjectBlockProps) => {
+  return (
+    <div className="Project-block">
+      <h2>{proj.name}</h2>
+      <h3>{proj.category}</h3>
+      <div className="Project-block__tags">
+        {proj.tags.map((tag, i) => {
+          return <p key={i}>{tag}</p>;
+        })}
+      </div>
+      <p>{proj.date}</p>
+      <div className="Project-block__content">
+        <p>{proj.detail.short}</p>
+      </div>
+    </div>
+  );
+};
+
+export default ProjectBlock;
