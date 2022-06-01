2413d61f5df8a9ef439d52e11b045f82f568ea19
Angus-Blade-Stealth <angushon96@gmail.com>
2022-06-01 00:24:13 -0700

    Nav-links__no-event to remove pointer events

--- src/pages/Landing.tsx
+++ src/pages/Landing.tsx
@@ -29,7 +29,7 @@ const Landing = ({ openSubLinks, setOpenSubLinks }: LandingProps) => {
       </div>
       <nav className="Landing-container__nav">
         <span>
-          <a>~$</a>
+          <a className="Nav-links__no-events">~$</a>
         </span>
         <span>
           <Link to="/default">default</Link>
