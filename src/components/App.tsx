dbb7467e494636552e68a5e38efe02bba05fe571
Fluffisaurus <angushon96@gmail.com>
2025-09-11 14:52:48 -0700

    refactor: move ThemeProvider to index.tsx
    Move ThemeProvider and CssBasline components to index.tsx. These are
    technically part of setup and styling related boilerplate code. Not
    directly related to App componenets/layouts.

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -1,12 +1,12 @@
 import React, { Suspense, lazy, useState } from "react";
 import { Routes, Route, useLocation, Navigate } from "react-router-dom";
-import { ThemeProvider } from "@mui/material/styles";
-import theme from "../mui/theme";
+import { Box } from "@mui/material";
 
 const Loading = lazy(() => import("../pages/Loading"));
 const Default = lazy(() => import("../pages/Default"));
 const DoesNotExist = lazy(() => import("../pages/DoesNotExist"));
 
+const CustomOptionsDial = lazy(() => import("./CustomOptionsDial"));
 const GlobalNav = lazy(() => import("../components/GlobalNav"));
 const Landing = lazy(() => import("../pages/Landing"));
 const Projects = lazy(() => import("../pages/Projects"));
@@ -21,7 +21,6 @@ const ProjectModalCarousel = lazy(
 );
 import { ProjectList } from "../content/projects/project-list";
 import { Project } from "../content/projects/interfaces";
-import { Box, CssBaseline } from "@mui/material";
 
 const App = () => {
   const [toggleNav, setToggleNav] = useState<boolean>(false);
@@ -31,56 +30,52 @@ const App = () => {
   const background = location.state && (location.state as any).background;
 
   return (
-    <ThemeProvider theme={theme} defaultMode="light" noSsr>
-      <CssBaseline />
-      <Box className="App">
-        <Suspense fallback={<Loading />}>
-          <GlobalNav toggleNav={toggleNav} setToggleNav={setToggleNav} />
-          <Box
-            className={!toggleNav ? "Container" : "Container Container--open"}
-          >
-            <Routes location={background || location}>
-              <Route path="/" element={<Landing />} />
-              <Route path="about" element={<AboutMe />} />
-              <Route path="projects" element={<Projects />}>
-                <Route index element={<Selection />} />
-                <Route path="personal" element={<Personal />} />
+    <Box className="App">
+      <CustomOptionsDial />
+      <Suspense fallback={<Loading />}>
+        <GlobalNav toggleNav={toggleNav} setToggleNav={setToggleNav} />
+        <Box className={!toggleNav ? "Container" : "Container Container--open"}>
+          <Routes location={background || location}>
+            <Route path="/" element={<Landing />} />
+            <Route path="about" element={<AboutMe />} />
+            <Route path="projects" element={<Projects />}>
+              <Route index element={<Selection />} />
+              <Route path="personal" element={<Personal />} />
+              <Route
+                path="personal/*"
+                element={<Navigate to="/projects/personal" replace />}
+              />
+              <Route path="academic" element={<Academic />} />
+              <Route
+                path="academic/*"
+                element={<Navigate to="/projects/academic" replace />}
+              />
+            </Route>
+            <Route path="contact" element={<Contact />} />
+            <Route path="default" element={<Default />} />
+            <Route path="*" element={<DoesNotExist />} />
+          </Routes>
+          {background && (
+            <Routes>
+              {ProjectList.personal.map((proj: Project, i) => (
                 <Route
-                  path="personal/*"
-                  element={<Navigate to="/projects/personal" replace />}
+                  key={i}
+                  path={`projects/personal/${proj.abbr}`}
+                  element={<ProjectModalCarousel proj={proj} />}
                 />
-                <Route path="academic" element={<Academic />} />
+              ))}
+              {ProjectList.academic.map((proj, i) => (
                 <Route
-                  path="academic/*"
-                  element={<Navigate to="/projects/academic" replace />}
+                  key={i}
+                  path={`projects/academic/${proj.abbr}`}
+                  element={<ProjectModalCarousel proj={proj} />}
                 />
-              </Route>
-              <Route path="contact" element={<Contact />} />
-              <Route path="default" element={<Default />} />
-              <Route path="*" element={<DoesNotExist />} />
+              ))}
             </Routes>
-            {background && (
-              <Routes>
-                {ProjectList.personal.map((proj: Project, i) => (
-                  <Route
-                    key={i}
-                    path={`projects/personal/${proj.abbr}`}
-                    element={<ProjectModalCarousel proj={proj} />}
-                  />
-                ))}
-                {ProjectList.academic.map((proj, i) => (
-                  <Route
-                    key={i}
-                    path={`projects/academic/${proj.abbr}`}
-                    element={<ProjectModalCarousel proj={proj} />}
-                  />
-                ))}
-              </Routes>
-            )}
-          </Box>
-        </Suspense>
-      </Box>
-    </ThemeProvider>
+          )}
+        </Box>
+      </Suspense>
+    </Box>
   );
 };
 
--- src/index.tsx
+++ src/index.tsx
@@ -1,8 +1,11 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import { BrowserRouter } from "react-router-dom";
+import { ThemeProvider } from "@mui/material/styles";
+import { CssBaseline } from "@mui/material";
 
 import App from "./components/App";
+import theme from "./mui/theme";
 
 import "./styles/base.scss";
 
@@ -12,7 +15,10 @@ const root = ReactDOM.createRoot(
 root.render(
   <React.StrictMode>
     <BrowserRouter>
-      <App />
+      <ThemeProvider theme={theme} noSsr>
+        <CssBaseline />
+        <App />
+      </ThemeProvider>
     </BrowserRouter>
   </React.StrictMode>
 );
