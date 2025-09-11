e5bdeea0491ee2ad4e404fe010a052384b4a30a0
Fluffisaurus <angushon96@gmail.com>
2025-09-11 16:55:55 -0700

    style: first attempt at dark theme

--- src/mui/muiColorSchemes.tsx
+++ src/mui/muiColorSchemes.tsx
@@ -4,6 +4,7 @@ const muiColorSchemes = {
       palette: {
         background: {
           default: "#fff3de",
+          paper: "white",
           highlight: "#147F8259",
           globalNav: "white",
         },
@@ -15,10 +16,31 @@ const muiColorSchemes = {
           main: "#aa51f73F",
           contrastText: "black",
         },
+        text: {
+          primary: "black",
+        },
       },
     },
     dark: {
-      palette: {},
+      palette: {
+        background: {
+          default: "#30383bff",
+          paper: "#1d2025",
+          highlight: "#147F8259",
+          globalNav: "black",
+        },
+        primary: {
+          main: "#53d8daff",
+          contrastText: "#fff3de",
+        },
+        secondary: {
+          main: "#9d2dff3f",
+          contrastText: "#fff3de",
+        },
+        text: {
+          primary: "#fff3de",
+        },
+      },
     },
   },
 };
