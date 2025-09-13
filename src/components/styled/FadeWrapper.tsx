fdc608df11ccd262a3712baaf783bbcf8a272115
Fluffisaurus <angushon96@gmail.com>
2025-09-12 17:03:07 -0700

    feat: Add optional delay logic in FadeWrapper
    FadeWrapper delays it's content shown if delay specified

--- src/components/styled/FadeWrapper.tsx
+++ src/components/styled/FadeWrapper.tsx
@@ -4,10 +4,18 @@ import Fade from "@mui/material/Fade";
 import { ANI_CONST, GenericWrapperProps } from "./constants";
 import { easing } from "@mui/material";
 
-const FadeWrapper = ({ children, timeout }: GenericWrapperProps) => {
+const FadeWrapper = ({ children, delay, timeout }: GenericWrapperProps) => {
+  const [showContent, setShowContent] = React.useState(false);
+  React.useEffect(() => {
+    const timer = setTimeout(() => {
+      setShowContent(true);
+    }, delay);
+    return () => clearTimeout(timer);
+  }, []);
+
   return (
     <Fade
-      in
+      in={delay ? showContent : true}
       timeout={timeout ? timeout : ANI_CONST.WRAPPER_FADE_WRAPPER_DELAY}
       easing={{ enter: easing.sharp, exit: easing.easeOut }}
     >
