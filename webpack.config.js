238c53c4de5abbc4002548564c33c7124ef720ef
Fluffisaurus <angushon96@gmail.com>
2025-09-16 19:30:09 -0700

    feat: optimize webpack vendor bundles
    Optimize webpack vendor bundles by splitting them by name. Inspo taken
    from https://stackoverflow.com/a/65860864

--- webpack.config.js
+++ webpack.config.js
@@ -66,6 +66,24 @@ module.exports = {
     minimizer: [new CssMinimizerPlugin()],
     splitChunks: {
       chunks: "all",
+      maxInitialRequests: Infinity,
+      minSize: 0,
+      cacheGroups: {
+        vendor: {
+          test: /[\\/]node_modules[\\/]/,
+          name(module) {
+            // https://stackoverflow.com/a/65860864
+            // get the name. E.g. node_modules/packageName/not/this/part.js
+            // or node_modules/packageName
+            const packageName = module.context.match(
+              /[\\/]node_modules[\\/](.*?)([\\/]|$)/
+            )[1];
+
+            // npm package names are URL-safe, but some servers don't like @ symbols
+            return `npm.${packageName.replace("@", "")}`;
+          },
+        },
+      },
     },
   },
   devServer: {
