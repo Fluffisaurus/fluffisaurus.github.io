ad30bbf7502cde08164aedc2a90950a1c60bcfe1
Fluffisaurus <angushon96@gmail.com>
2025-09-21 22:03:01 -0700

    perf: lazy load ScrollArea from @blur-ui/scroll-area

--- src/components/styled/ScrollableContainer.tsx
+++ src/components/styled/ScrollableContainer.tsx
@@ -1,7 +1,9 @@
 import * as React from "react";
+import loadable from "@loadable/component";
 
 import { Container, SxProps, Theme } from "@mui/material";
-import { ScrollArea } from "@blur-ui/scroll-area";
+
+const ScrollArea = loadable.lib(() => import("@blur-ui/scroll-area"));
 
 interface ScrollableContainerProps {
   children: React.ReactNode;
@@ -18,23 +20,27 @@ const ScrollableContainer = ({
 }: ScrollableContainerProps) => {
   return (
     <Container sx={sx} className={className} ref={ref}>
-      <ScrollArea
-        classNames={{
-          horizontalScrollbar: "h-2.5",
-          root: "w-60 h-60 text-black dark:text-white",
-          scrollbar: "p-[1px]",
-          thumb:
-            "bg-neutral-800 dark:bg-neutral-100 rounded-full opacity-30 hover:opacity-40 transition-opacity",
-          verticalScrollbar: "w-2.5",
-        }}
-        dir="ltr"
-        orientation="vertical"
-        scrollHideDelay={600}
-        shadowSize={50}
-        type="always"
-        style={{ width: "100%", height: "100%" }}
-      >
-        {children}
+      <ScrollArea>
+        {(ScrollAreaModule: typeof import("@blur-ui/scroll-area")) => (
+          <ScrollAreaModule.ScrollArea
+            classNames={{
+              horizontalScrollbar: "h-2.5",
+              root: "w-60 h-60 text-black dark:text-white",
+              scrollbar: "p-[1px]",
+              thumb:
+                "bg-neutral-800 dark:bg-neutral-100 rounded-full opacity-30 hover:opacity-40 transition-opacity",
+              verticalScrollbar: "w-2.5",
+            }}
+            dir="ltr"
+            orientation="vertical"
+            scrollHideDelay={600}
+            shadowSize={50}
+            type="always"
+            style={{ width: "100%", height: "100%" }}
+          >
+            {children}
+          </ScrollAreaModule.ScrollArea>
+        )}
       </ScrollArea>
     </Container>
   );
