2e5ad32c6b989e9ffcc461c61d7e5e0a84bd9be0
Fluffisaurus <angushon96@gmail.com>
2026-01-16 18:51:50 -0800

    feat: update loading page with typing animation and custom font

--- src/pages/Loading.tsx
+++ src/pages/Loading.tsx
@@ -1,13 +1,29 @@
 import React from "react";
-import FadeWrapper from "../components/styled/FadeWrapper";
+import Box from "@mui/material/Box";
 import Typography from "@mui/material/Typography";
+import { TypeAnimation } from "react-type-animation";
 
 const Loading: React.FunctionComponent = () => {
   return (
     <>
-      <FadeWrapper>
-        <Typography variant="h5">loading...</Typography>
-      </FadeWrapper>
+      <Box
+        sx={{
+          width: "100%",
+          height: "100%",
+          display: "flex",
+          justifyContent: "center",
+          alignItems: "center",
+        }}
+      >
+        <Typography variant="subtitle1" fontSize={32}>
+          <TypeAnimation
+            key={"loading-screen"}
+            sequence={[500, "loading...", 1500, "loading"]}
+            speed={{ type: "keyStrokeDelayInMs", value: 100 }}
+            repeat={Infinity}
+          />
+        </Typography>
+      </Box>
     </>
   );
 };
