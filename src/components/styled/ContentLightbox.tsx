a8c8fb7e1fdfc8e50eb785d18eba995559b3a6e4
Fluffisaurus <angushon96@gmail.com>
2026-03-24 21:30:51 -0700

    chore: custom mui icons for lightbox and some config

--- src/components/styled/ContentLightbox.tsx
+++ src/components/styled/ContentLightbox.tsx
@@ -1,10 +1,27 @@
 import * as React from "react";
 
+import ArrowForwardIosTwoToneIcon from "@mui/icons-material/ArrowForwardIosTwoTone";
+import ArrowBackIosTwoToneIcon from "@mui/icons-material/ArrowBackIosTwoTone";
+import ZoomOutTwoToneIcon from "@mui/icons-material/ZoomOutTwoTone";
+import ZoomInTwoToneIcon from "@mui/icons-material/ZoomInTwoTone";
+import CloseTwoToneIcon from "@mui/icons-material/CloseTwoTone";
+import PhotoLibraryTwoToneIcon from "@mui/icons-material/PhotoLibraryTwoTone";
+import CallToActionTwoToneIcon from "@mui/icons-material/CallToActionTwoTone";
+import WebAssetOffTwoToneIcon from "@mui/icons-material/WebAssetOffTwoTone";
+import ClosedCaptionTwoToneIcon from "@mui/icons-material/ClosedCaptionTwoTone";
+import ClosedCaptionDisabledTwoToneIcon from "@mui/icons-material/ClosedCaptionDisabledTwoTone";
+import FullscreenTwoToneIcon from "@mui/icons-material/FullscreenTwoTone";
+import CloseFullscreenTwoToneIcon from "@mui/icons-material/CloseFullscreenTwoTone";
+import PlayArrowTwoToneIcon from "@mui/icons-material/PlayArrowTwoTone";
+import PauseTwoToneIcon from "@mui/icons-material/PauseTwoTone";
+
 import Lightbox from "yet-another-react-lightbox";
 import Captions from "yet-another-react-lightbox/plugins/captions";
 import Zoom from "yet-another-react-lightbox/plugins/zoom";
 import Thumbnails from "yet-another-react-lightbox/plugins/thumbnails";
 import Counter from "yet-another-react-lightbox/plugins/counter";
+import Fullscreen from "yet-another-react-lightbox/plugins/fullscreen";
+import Slideshow from "yet-another-react-lightbox/plugins/slideshow";
 import "yet-another-react-lightbox/styles.css";
 import "yet-another-react-lightbox/plugins/captions.css";
 import "yet-another-react-lightbox/plugins/thumbnails.css";
@@ -29,12 +46,31 @@ const ContentLightbox = (props: ContentLightboxProps) => {
   return (
     <>
       <Lightbox
-        plugins={[Captions, Zoom, Thumbnails, Counter]}
+        plugins={[Counter, Fullscreen, Slideshow, Thumbnails, Captions, Zoom]}
         open={open}
         close={() => setOpen(!open)}
         slides={CloudinaryCustomUrl({ images, imgQuality })}
         index={slideIndex}
         on={{ view: ({ index: currentIndex }) => setSlideIndex(currentIndex) }}
+        thumbnails={{ showToggle: true }}
+        captions={{ showToggle: true }}
+        slideshow={{ autoplay: false, delay: 3000 }}
+        render={{
+          iconZoomOut: () => <ZoomOutTwoToneIcon />,
+          iconZoomIn: () => <ZoomInTwoToneIcon />,
+          iconClose: () => <CloseTwoToneIcon />,
+          iconNext: () => <ArrowForwardIosTwoToneIcon />,
+          iconPrev: () => <ArrowBackIosTwoToneIcon />,
+          iconError: () => <PhotoLibraryTwoToneIcon />,
+          iconThumbnailsVisible: () => <CallToActionTwoToneIcon />,
+          iconThumbnailsHidden: () => <WebAssetOffTwoToneIcon />,
+          iconCaptionsVisible: () => <ClosedCaptionTwoToneIcon />,
+          iconCaptionsHidden: () => <ClosedCaptionDisabledTwoToneIcon />,
+          iconEnterFullscreen: () => <FullscreenTwoToneIcon />,
+          iconExitFullscreen: () => <CloseFullscreenTwoToneIcon />,
+          iconSlideshowPlay: () => <PlayArrowTwoToneIcon />,
+          iconSlideshowPause: () => <PauseTwoToneIcon />,
+        }}
       />
     </>
   );
