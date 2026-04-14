304c6f3ac2587b54552633798ba8dac469344b42
Fluffisaurus <angushon96@gmail.com>
2026-04-13 18:51:26 -0700

    refactor: use native browser pdf w/ object tag to reduce bundle size

--- src/pages/Resume.tsx
+++ src/pages/Resume.tsx
@@ -4,7 +4,7 @@ import Box from "@mui/material/Box";
 import Typography from "@mui/material/Typography";
 
 import FadeWrapper from "../components/styled/FadeWrapper";
-import PdfViewer from "../components/pdf/PdfViewer";
+// import PdfViewer from "../components/pdf/PdfViewer";
 
 const Resume: React.FunctionComponent = () => {
   return (
@@ -15,12 +15,23 @@ const Resume: React.FunctionComponent = () => {
           flexDirection: "column",
           flex: "1 1 0",
           overflow: "hidden",
+          height: "100%",
         }}
       >
         <FadeWrapper>
           <Typography variant="h3">Resume</Typography>
         </FadeWrapper>
-        <PdfViewer />
+        {/* <PdfViewer /> */}
+        <object
+          data="https://res.cloudinary.com/djlcricbu/image/upload/v1653627212/pdfs/placeholder-pdf-doc_j3pjxs.pdf"
+          type="application/pdf"
+          width="100%"
+          height="100%"
+        >
+          <Typography variant="body1">
+            Your browser does not support PDF viewing.
+          </Typography>
+        </object>
       </Box>
     </>
   );
